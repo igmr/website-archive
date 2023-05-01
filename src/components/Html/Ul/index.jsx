0e775d73f778e6a3f8f216d0aef738c2d6c11a7d
Ivan Martinez <igmrivan@gmail.com>
2023-05-01 12:58:16 -0600

    Update component Ul

--- src/components/Html/Ul/index.jsx
+++ src/components/Html/Ul/index.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function Ul({ children }) {
   return (
-    <ul style={{ listStyle: "none", padding: 0, margin: 0 }}>{children}</ul>
+    <ul style={{ listStyle: "none", margin: 0 }}>{children}</ul>
   );
 }
